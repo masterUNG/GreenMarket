4bac8d8ce3676b63eecb8c8cc94fb75328f5baa1
masterUNG <phrombutr@gmail.com>
2020-10-16 09:02:54 +0700

    before

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:greenmarket/screens/home.dart';
 
-main() => runApp(MyApp()); 
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData(primarySwatch: Colors.green),
-      
-
-    title: 'green market',
-    home:Home(),
-  );
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      title: 'green market',
+      home: Home(),
+    );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,6 +1,6 @@
 class MyConstant {
   // String domain =  'http://10.120.1.110:/602021197';
-  String domain = 'https://d131d09b2911.ngrok.io';
+  String domain = 'https://bb6e287812ee.ngrok.io';
 
   MyConstant();
 }
